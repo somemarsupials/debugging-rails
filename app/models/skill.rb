a9e6174403b2d5a09fb435e7180f522316612e3d
Theo Breuer-Weil <theobreuerweil@gmail.com>
2017-12-12 11:13:05 +0000

    Add documentation to Skill model

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,3 +1,9 @@
+##
+# Skill represents abilities that individual users can have.
+# 
+# Skills have a name. They exist in a many-to-many relationship
+# with users, through the join table 'users_skills'.
+
 class Skill < ActiveRecord::Base
 
   has_many :users_skills
